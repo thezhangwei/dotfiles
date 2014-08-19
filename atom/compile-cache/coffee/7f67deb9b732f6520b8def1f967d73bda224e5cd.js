2b3f779c452eae70120292b01063511d91e62a40
Wei Zhang <thezhangwei@gmail.com>
2014-08-19 15:23:27 -0700

    Migrate to use python package dotfiles to manage this repo

---
+++ atom/compile-cache/coffee/7f67deb9b732f6520b8def1f967d73bda224e5cd.js
@@ -0,0 +1,7 @@
+(function() {
+
+
+}).call(this);
+
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAiZmlsZSI6ICIiLAogICJzb3VyY2VSb290IjogIiIsCiAgInNvdXJjZXMiOiBbCiAgICAiIgogIF0sCiAgIm5hbWVzIjogW10sCiAgIm1hcHBpbmdzIjogIkFBbUJzRDtBQUFBO0FBQUEiCn0=
+//# sourceURL=/Users/zwei/.atom/init.coffee
